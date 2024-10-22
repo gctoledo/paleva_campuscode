d592cd24f923ffc677c4095511c121d22d4348c4
gctoledo <gabrielcostatoledo@outlook.com>
2024-10-22 19:41:03 -0300

    feat: add custom layout to auth pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  devise_for :users
+  devise_for :users, controllers: {
+  sessions: 'users/sessions',
+  registrations: 'users/registrations'
+}
+
   root to: "dashboard#index"
 end
