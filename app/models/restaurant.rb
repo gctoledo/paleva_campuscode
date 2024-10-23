9dd5269bc6a97cb7ec3838d94a818be6a88589e7
gctoledo <gabrielcostatoledo@outlook.com>
2024-10-22 23:10:26 -0300

    feat: add validation to Restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,23 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
+
+  validates :trade_name, :legal_name, :cnpj, :address, :phone, :email, presence: true
+  
+  validates :cnpj, uniqueness: true
+  validate :cnpj_must_be_valid
+
+  before_create :generate_unique_code
+
+  private
+
+  def cnpj_must_be_valid
+    errors.add(:cnpj, "não é válido") unless CNPJ.valid?(cnpj)
+  end
+
+  def generate_unique_code
+    self.code = loop do
+      random_code = SecureRandom.alphanumeric(6).upcase
+      break random_code unless Restaurant.exists?(code: random_code)
+    end
+  end
 end
