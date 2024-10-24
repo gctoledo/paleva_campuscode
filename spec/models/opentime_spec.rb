be19db8c2e31ad8e3983c468ccf2f6e0bf7e1bc8
gctoledo <gabrielcostatoledo@outlook.com>
2024-10-24 19:33:54 -0300

    test: add initial tests to opentime model

--- spec/models/opentime_spec.rb
+++ spec/models/opentime_spec.rb
@@ -1,4 +1,97 @@
 require 'rails_helper'
 
 RSpec.describe Opentime, type: :model do
+  describe '#valid?' do
+    it 'false when week_day is empty' do
+      #Arrange
+      user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
+      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
+      
+      #Act
+      opentime = restaurant.opentimes.new(open: '08:30', close: '18:00')
+      result = opentime.valid?
+
+      #Assert
+      expect(result).to eq false
+    end
+
+    it 'false when open is empty' do
+      #Arrange
+      user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
+      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
+      
+      #Act
+      opentime = restaurant.opentimes.new(week_day: 0, close: '18:00')
+      result = opentime.valid?
+
+      #Assert
+      expect(result).to eq false
+    end
+
+    it 'false when close is empty' do
+      #Arrange
+      user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
+      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
+      
+      #Act
+      opentime = restaurant.opentimes.new(week_day: 0, open: '08:30')
+      result = opentime.valid?
+
+      #Assert
+      expect(result).to eq false
+    end
+
+    it 'false when close time is less than open time' do
+      #Arrange
+      user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
+      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
+      
+      #Act
+      opentime = restaurant.opentimes.new(week_day: 0, open: '18:00', close: '08:30')
+      result = opentime.valid?
+
+      #Assert
+      expect(result).to eq false
+    end
+
+    it 'false when week day already exists' do
+      #Arrange
+      user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
+      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
+      restaurant.opentimes.new(week_day: 0, open: '18:00', close: '08:30')
+      
+      #Act
+      opentime = restaurant.opentimes.new(week_day: 0, open: '18:00', close: '08:30')
+      result = opentime.valid?
+
+      #Assert
+      expect(result).to eq false
+    end
+
+    it 'false when week day is different than 0..6' do
+      #Arrange
+      user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
+      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
+      
+      #Act
+      opentime = restaurant.opentimes.new(week_day: 10, open: '18:00', close: '08:30')
+      result = opentime.valid?
+
+      #Assert
+      expect(result).to eq false
+    end
+
+    it 'with success' do
+      #Arrange
+      user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
+      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
+      
+      #Act
+      opentime = restaurant.opentimes.new(week_day: 0, open: '08:30', close: '18:00')
+      result = opentime.valid?
+
+      #Assert
+      expect(result).to eq true
+    end
+  end
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe Restaurant, type: :model do
     it 'false when trade_name is empty' do
       #Arrange
       user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
-      restaurant = Restaurant.new(legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
-
+      
       #Act
+      restaurant = Restaurant.new(legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
       result = restaurant.valid?
 
       #Assert
@@ -17,9 +17,9 @@ RSpec.describe Restaurant, type: :model do
     it 'false when legal_name is empty' do
       #Arrange
       user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
-      restaurant = Restaurant.new(trade_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
-
+      
       #Act
+      restaurant = Restaurant.new(trade_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
       result = restaurant.valid?
 
       #Assert
@@ -29,9 +29,9 @@ RSpec.describe Restaurant, type: :model do
     it 'false when cnpj is empty' do
       #Arrange
       user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
-      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
-
+      
       #Act
+      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
       result = restaurant.valid?
 
       #Assert
@@ -41,9 +41,9 @@ RSpec.describe Restaurant, type: :model do
     it 'false when address is empty' do
       #Arrange
       user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
-      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, phone: '11111111111', email: 'burger@king.com', user: user)
-
+      
       #Act
+      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, phone: '11111111111', email: 'burger@king.com', user: user)
       result = restaurant.valid?
 
       #Assert
@@ -53,9 +53,9 @@ RSpec.describe Restaurant, type: :model do
     it 'false when phone is empty' do
       #Arrange
       user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
-      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', email: 'burger@king.com', user: user)
-
+      
       #Act
+      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', email: 'burger@king.com', user: user)
       result = restaurant.valid?
 
       #Assert
@@ -65,9 +65,9 @@ RSpec.describe Restaurant, type: :model do
     it 'false when email is empty' do
       #Arrange
       user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
-      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', user: user)
-
+      
       #Act
+      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', user: user)
       result = restaurant.valid?
 
       #Assert
@@ -78,11 +78,10 @@ RSpec.describe Restaurant, type: :model do
       #Arrange
       first_user = User.create!(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
       first_restaurant = Restaurant.create!(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: first_user)
-      
       second_user = User.new(email: 'mary@jane.com', cpf: CPF.generate, first_name: 'Mary', last_name: 'Jane', password: 'password123456')
-      restaurant = Restaurant.new(trade_name: 'McDonalds', legal_name: 'McDonalds', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: first_restaurant.email, user: second_user)
-
+      
       #Act
+      restaurant = Restaurant.new(trade_name: 'McDonalds', legal_name: 'McDonalds', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: first_restaurant.email, user: second_user)
       result = restaurant.valid?
 
       #Assert
@@ -93,11 +92,10 @@ RSpec.describe Restaurant, type: :model do
       #Arrange
       first_user = User.create!(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
       first_restaurant = Restaurant.create!(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: first_user)
-
       second_user = User.new(email: 'mary@jane.com', cpf: CPF.generate, first_name: 'Mary', last_name: 'Jane', password: 'password123456')
-      restaurant = Restaurant.new(trade_name: 'McDonalds', legal_name: 'McDonalds', cnpj: first_restaurant.cnpj, address: 'United Stated', phone: '11111111111', email: 'mc@donalds.com', user: second_user)
 
       #Act
+      restaurant = Restaurant.new(trade_name: 'McDonalds', legal_name: 'McDonalds', cnpj: first_restaurant.cnpj, address: 'United Stated', phone: '11111111111', email: 'mc@donalds.com', user: second_user)
       result = restaurant.valid?
 
       #Assert
@@ -110,9 +108,9 @@ RSpec.describe Restaurant, type: :model do
       first_restaurant = Restaurant.create!(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: first_user)
 
       second_user = User.new(email: 'mary@jane.com', cpf: CPF.generate, first_name: 'Mary', last_name: 'Jane', password: 'password123456')
-      restaurant = Restaurant.new(trade_name: 'McDonalds', legal_name: first_restaurant.legal_name, cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'mc@donalds.com', user: second_user)
 
       #Act
+      restaurant = Restaurant.new(trade_name: 'McDonalds', legal_name: first_restaurant.legal_name, cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'mc@donalds.com', user: second_user)
       result = restaurant.valid?
 
       #Assert
@@ -122,9 +120,9 @@ RSpec.describe Restaurant, type: :model do
     it 'false when phone is smaller then 10 numbers' do
       #Arrange
       user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
-      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '123456789', email: 'burger@king.com', user: user)
-
+      
       #Act
+      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '123456789', email: 'burger@king.com', user: user)
       result = restaurant.valid?
 
       #Assert
@@ -134,9 +132,9 @@ RSpec.describe Restaurant, type: :model do
     it 'false when phone is bigger then 11 numbers' do
       #Arrange
       user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
-      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '123456789012', email: 'burger@king.com', user: user)
-
+      
       #Act
+      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '123456789012', email: 'burger@king.com', user: user)
       result = restaurant.valid?
 
       #Assert
@@ -146,9 +144,9 @@ RSpec.describe Restaurant, type: :model do
     it 'false when email is not valid' do
       #Arrange
       user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
-      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'invalid_email', user: user)
-
+      
       #Act
+      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'invalid_email', user: user)
       result = restaurant.valid?
 
       #Assert
@@ -158,9 +156,9 @@ RSpec.describe Restaurant, type: :model do
     it 'false when cnpj is not valid' do
       #Arrange
       user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
-      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: 'invalid_cnpj', address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
-
+      
       #Act
+      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: 'invalid_cnpj', address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
       result = restaurant.valid?
 
       #Assert
@@ -170,9 +168,9 @@ RSpec.describe Restaurant, type: :model do
     it 'creates unique random code' do
       #Arrange
       user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
-      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
-
+      
       #Act
+      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
       code = restaurant.code
 
       #Assert
@@ -183,9 +181,9 @@ RSpec.describe Restaurant, type: :model do
     it 'with success' do
       #Arrange
       user = User.new(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
-      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
-
+      
       #Act
+      restaurant = Restaurant.new(trade_name: 'Burguer King', legal_name: 'Burguer King', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'burger@king.com', user: user)
       result = restaurant.valid?
 
       #Assert
