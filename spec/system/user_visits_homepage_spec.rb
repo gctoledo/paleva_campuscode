6631393078ff488a288557b6dc5bba3df186abde
gctoledo <gabrielcostatoledo@outlook.com>
2024-10-24 16:58:19 -0300

    test: ensure that the logout button logs out

--- spec/system/user_visits_homepage_spec.rb
+++ spec/system/user_visits_homepage_spec.rb
@@ -26,6 +26,19 @@ describe 'User visits homepage' do
     expect(page).to have_content('Você precisa cadastrar seu restaurante antes de continuar.')
   end
 
+  it 'and logout' do
+    #Arrange
+    
+    #Act
+    user = User.create!(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
+    login_as(user)
+    visit root_path
+    click_on 'Sair'
+
+    #Assert
+    expect(current_path).to eq new_user_session_path
+  end
+
   it 'with success' do
     #Arrange
     user = User.create!(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
