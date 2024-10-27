b0a95cc469370ccb9f4f099ff2573c70321f795d
gctoledo <gabrielcostatoledo@outlook.com>
2024-10-27 01:05:33 -0300

    test: add test to dishes access

--- spec/system/user_creates_dish_spec.rb
+++ spec/system/user_creates_dish_spec.rb
@@ -21,6 +21,43 @@ describe 'User visits dish creation page' do
     expect(page).to have_field('Imagem')
   end
 
+  it 'cant access dishes from other restaurants' do
+    #Arrange
+    first_user = User.create!(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
+    second_user = User.create!(email: 'mary@jane.com', cpf: CPF.generate, first_name: 'Mary', last_name: 'Jane', password: 'password123456')
+    Restaurant.create!(trade_name: 'McDonalds', legal_name: 'McDonalds', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'mc@donalds.com', user: second_user)
+    login_as(second_user)
+    create_restaurant(first_user)
+    create_opentime(first_user)
+    create_opentime(second_user)
+    dish = first_user.restaurant.dishes.create!(name: 'Parmegiana', description: 'É bom!')
+
+    #Act
+    visit dish_path(dish.id)
+
+    #Assert
+    expect(current_path).to eq root_path
+    expect(page).to have_content('Acesso não autorizado.')
+  end
+
+  it 'can access your dishes' do
+    #Arrange
+    user = User.create!(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
+    login_as(user)
+    create_restaurant(user)
+    create_opentime(user)
+    dish = user.restaurant.dishes.create!(name: 'Parmegiana', description: 'É bom!')
+
+    #Act
+    visit root_path
+    click_on 'Pratos'
+    click_on 'Parmegiana'
+
+    #Assert
+    expect(current_path).to eq dish_path(dish.id)
+    expect(page).to have_content('Parmegiana')
+  end
+
   it 'and cant create dish with incorrect params' do
     #Arrange
     user = User.create!(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
