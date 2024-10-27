b01b9972a32170d0aea89ee1a137dd846d1f07c2
gctoledo <gabrielcostatoledo@outlook.com>
2024-10-27 02:10:47 -0300

    refactor: separate tests by controller

--- spec/system/dish/user_creates_dish_spec.rb
+++ spec/system/dish/user_creates_dish_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'User visits dish page' do
+describe 'User visits dish creation page' do
   it 'and sees all form inputs in creation form' do
     #Arrange
     user = User.create!(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
@@ -19,55 +19,6 @@ describe 'User visits dish page' do
     expect(page).to have_field('Descrição')
     expect(page).to have_field('Calorias')
     expect(page).to have_field('Imagem')
-  end
-
-  it 'and cant access dishes from other restaurants' do
-    #Arrange
-    first_user = User.create!(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
-    second_user = User.create!(email: 'mary@jane.com', cpf: CPF.generate, first_name: 'Mary', last_name: 'Jane', password: 'password123456')
-    Restaurant.create!(trade_name: 'McDonalds', legal_name: 'McDonalds', cnpj: CNPJ.generate, address: 'United Stated', phone: '11111111111', email: 'mc@donalds.com', user: second_user)
-    login_as(second_user)
-    create_restaurant(first_user)
-    create_opentime(first_user)
-    create_opentime(second_user)
-    dish = first_user.restaurant.dishes.new(name: 'Parmegiana', description: 'É bom!')
-    dish.image.attach(
-      io: File.open('spec/fixtures/test_image.png'),
-      filename: 'test_image.png',
-      content_type: 'image/png'
-    )
-    dish.save
-
-    #Act
-    visit dish_path(dish.id)
-
-    #Assert
-    expect(current_path).to eq root_path
-    expect(page).to have_content('Acesso não autorizado.')
-  end
-
-  it 'and can access your dishes' do
-    #Arrange
-    user = User.create!(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
-    login_as(user)
-    create_restaurant(user)
-    create_opentime(user)
-    dish = user.restaurant.dishes.new(name: 'Parmegiana', description: 'É bom!')
-    dish.image.attach(
-      io: File.open('spec/fixtures/test_image.png'),
-      filename: 'test_image.png',
-      content_type: 'image/png'
-    )
-    dish.save
-
-    #Act
-    visit root_path
-    click_on 'Pratos'
-    click_on 'Parmegiana'
-
-    #Assert
-    expect(current_path).to eq dish_path(dish.id)
-    expect(page).to have_content('Parmegiana')
   end
 
   it 'and cant create dish with incorrect params' do
