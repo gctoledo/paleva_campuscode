ffd8c173cd6459d69b76d8e4572c28d9a099eaab
gctoledo <gabrielcostatoledo@outlook.com>
2024-10-27 23:49:11 -0300

    feat: add initial tests to seach

---
+++ spec/system/home/user_searchs_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+describe 'User searchs' do
+  it 'from navbar in homepage' do
+    #Arrange
+    user = User.create!(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
+    login_as(user)
+    create_restaurant(user)
+    create_opentime(user)
+  
+    drink = user.restaurant.drinks.new(name: 'Coca-cola', description: 'Refrigerante de cola.')
+    drink.image.attach(
+      io: File.open('spec/fixtures/test_image.png'),
+      filename: 'test_image.png',
+      content_type: 'image/png'
+    )
+    drink.save
+
+    dish = user.restaurant.dishes.new(name: 'Costela', description: 'É bom!')
+    dish.image.attach(
+      io: File.open('spec/fixtures/test_image.png'),
+      filename: 'test_image.png',
+      content_type: 'image/png'
+    )
+    dish.save
+    
+    #Act
+    visit root_path
+    within('#search-form') do
+      fill_in 'Buque por pratos ou bebidas:', with: 'Co'
+    end
+    click_on 'Buscar'
+
+    #Assert
+    expect(current_path).to eq search_path
+    expect(page).to have_content('Coca-cola')
+    expect(page).to have_content('Costela')
+  end
+
+  it 'and cant find anything' do
+    #Arrange
+    user = User.create!(email: 'john@doe.com', cpf: CPF.generate, first_name: 'John', last_name: 'Doe', password: 'password123456')
+    login_as(user)
+    create_restaurant(user)
+    create_opentime(user)
+    
+    #Act
+    visit root_path
+    within('#search-form') do
+      fill_in 'Buque por pratos ou bebidas:', with: 'Co'
+    end
+    click_on 'Buscar'
+
+    #Assert
+    expect(current_path).to eq search_path
+    expect(page).to have_content('Nenhum cadastro encontrado!')
+  end
+end
