ff36a3fe76c200dbeac51a3d548c33afafae7a02
gctoledo <gabrielcostatoledo@outlook.com>
2024-11-01 00:27:07 -0300

    feat: add Portion validation

--- app/models/portion.rb
+++ app/models/portion.rb
@@ -4,11 +4,21 @@ class Portion < ApplicationRecord
 
   before_update :store_price_history
 
+  validates :description, :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validate :price_must_have_two_decimal_places
+
   private
 
   def store_price_history
     if price_changed?
       price_histories.create(price: price_was, changed_at: Time.current)
+    end
+  end
+
+  def price_must_have_two_decimal_places
+    if price.present? && (price * 100) % 1 != 0
+      errors.add(:price, "deve ter no máximo duas casas decimais.")
     end
   end
 end
