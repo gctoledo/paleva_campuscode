b429bb4222558ce3ce6f07035d8cf96f05355c60
gctoledo <gabrielcostatoledo@outlook.com>
2024-11-19 13:45:25 -0300

    chore: add start_date and end_date columns to menus table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_18_202054) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_19_125158) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -95,6 +95,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_18_202054) do
     t.integer "restaurant_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "start_date"
+    t.date "end_date"
     t.index ["restaurant_id", "name"], name: "index_menus_on_restaurant_id_and_name", unique: true
     t.index ["restaurant_id"], name: "index_menus_on_restaurant_id"
   end
